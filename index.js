ed1f38b0583d1298149bfeb096c71b417c46248e
mattdesl <dave.des@gmail.com>
2014-10-17 13:21:35 -0400

    using classes as optimization

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var Base = require('fontpath-renderer')
 
+//TODO: Eventually lots of this code will just replace fontpath-renderer...
+
 function FontpathRenderer(options) {
     if (!(this instanceof FontpathRenderer))
         return new FontpathRenderer(options)
@@ -20,6 +22,11 @@ function FontpathRenderer(options) {
         this.wordwrap.mode = options.wrapMode
     if (typeof options.wrapWidth === 'number')
         this.layout(options.wrapWidth)
+
+    this.data = {
+        glyphs: [],
+        underlines: []
+    }
 }
 
 FontpathRenderer.prototype = Object.create(Base.prototype)
@@ -27,30 +34,38 @@ FontpathRenderer.constructor = FontpathRenderer
 FontpathRenderer.Align = Base.Align
 
 FontpathRenderer.prototype.renderGlyph = function(i, glyph, scale, x, y) {
-    this.data.glyphs.push({
-        glyph: glyph,
-        charCode: this.text.charCodeAt(i),
-        scale: [ scale, -scale ],
-        position: [ x, y ],
-        index: i
-    })
+    this.data.glyphs.push(new Glyph(i, glyph, 
+                this.text.charCodeAt(i), 
+                [ scale, this.font.bitmap ? scale : -scale ],
+                [ x, y ]))
 }
 
 FontpathRenderer.prototype.renderUnderline = function(x, y, width, height) {
-    this.data.underlines.push({
-        position: [ x, y ],
-        size: [ width, height ]
-    })
+    this.data.underlines.push(new Underline(
+        [ x, y ],
+        [ width, height ]
+    ))
 }
 
 FontpathRenderer.prototype.render = function(x, y, start, end) {
     //new data for result
-    this.data = {
-        glyphs: [],
-        underlines: []
-    }
+    this.data.glyphs.length = 0
+    this.data.underlines.length = 0
     Base.prototype.render.call(this, x, y, start, end)
     return this.data
+}
+
+function Glyph(index, glyph, charCode, scale, position) {
+    this.glyph = glyph
+    this.index = index
+    this.charCode = charCode
+    this.position = position
+    this.scale = scale
+}
+
+function Underline(position, size) {
+    this.position = position
+    this.size = size
 }
 
 module.exports = FontpathRenderer
